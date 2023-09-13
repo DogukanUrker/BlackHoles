ae3b3d9b232118bb8598ab913be5ca037f76ec97
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-13 22:30:02 +0300

    created "/blachole/{id}" for rendering specific blackholes

--- src/views/blackHole.tsx
+++ src/views/blackHole.tsx
@@ -1,7 +1,18 @@
+import axios from "axios";
+import { API } from "../constants";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { blackholeComponent } from "../components/blackhole";
 export default function blackhole() {
-  return (
-    <div>
-      <h1>BlackHole</h1>
-    </div>
-  );
+  let { id } = useParams();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get(`${API}data/${id}`).then((json) => setData(json.data));
+  }, []);
+  const renderData = () => {
+    return data.map((data) => {
+      return blackholeComponent(data);
+    });
+  };
+  return <div className="text-neutral-50">{renderData()}</div>;
 }
