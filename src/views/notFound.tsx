becb3e98b449892616f3fa955c12ce5ab8b55798
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-14 23:45:46 +0300

    404 screen updated

--- src/views/notFound.tsx
+++ src/views/notFound.tsx
@@ -1,8 +1,12 @@
 export default function NotFound() {
   return (
     <>
-      <div className="w-fit mx-auto text-center mt-16 select-none">
-        <p className="text-[8rem] text-neutral-50 font-semibold">404</p>
+      <div className="w-fit mx-auto mt-32 p-2 text-left select-none  text-neutral-50 font-semibold">
+        <p className="text-4xl">Hmm.</p>
+        <p className="text-xl mb-6 ">Looks like fell into a Black Hole. 😕</p>
+        <a href="/" className="text-center hover:text-neutral-300 duration-150">
+          Click to return to the Milky Way. 🌌
+        </a>
       </div>
     </>
   );
