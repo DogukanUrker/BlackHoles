850b5b88a8cbf477c23eac5eed2f935be2535d5d
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-14 23:46:24 +0300

    added simple landing page

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,7 +1,22 @@
 export default function index() {
   return (
-    <div>
-      <h1>Index</h1>
+    <div className="body">
+      <div className="rounded-md w-fit mx-auto mt-32 p-2 text-center select-none  text-neutral-50 font-semibold">
+        <p className="text-4xl mb-6">Black Holes</p>
+        <a
+          href="/blackholes"
+          className="text-center hover:text-neutral-300 duration-150"
+        >
+          Click to see all Black Holes.
+        </a>{" "}
+        <br />
+        <a
+          href="/go"
+          className="text-center hover:text-neutral-300 duration-150"
+        >
+          Click to find Black Holes by ID.
+        </a>
+      </div>
     </div>
   );
 }
