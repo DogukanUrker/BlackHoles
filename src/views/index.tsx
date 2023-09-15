06101ba5b319af53c37d982629337cd095b1f8cf
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-15 19:48:46 +0300

    added URL for "/random"

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,17 +2,25 @@ export default function index() {
   return (
     <div className="body">
       <div className="rounded-md w-fit mx-auto mt-32 p-2 text-center select-none  text-neutral-50 font-semibold">
-        <p className="text-4xl mb-6">Black Holes</p>
+        <p className="text-5xl mb-6">Black Holes</p>
+        <br />
         <a
           href="/blackholes"
-          className="text-center hover:text-neutral-300 duration-150"
+          className="text-center hover:text-neutral-300 duration-150 text-xl"
         >
           Click to see all Black Holes.
         </a>{" "}
         <br />
         <a
+          href="/random"
+          className="text-center hover:text-neutral-300 duration-150 text-xl"
+        >
+          Click for a random Black Hole.
+        </a>
+        <br />
+        <a
           href="/go"
-          className="text-center hover:text-neutral-300 duration-150"
+          className="text-center hover:text-neutral-300 duration-150 text-xl"
         >
           Click to find Black Holes by ID.
         </a>
