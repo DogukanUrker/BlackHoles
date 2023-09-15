3302734712c2e865c98beb309bddcc45fd106956
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-15 19:56:13 +0300

    added GIF background for 404 screen

--- src/views/notFound.tsx
+++ src/views/notFound.tsx
@@ -1,12 +1,17 @@
 export default function NotFound() {
   return (
     <>
-      <div className="w-fit mx-auto mt-32 p-2 text-left select-none  text-neutral-50 font-semibold">
-        <p className="text-4xl">Hmm.</p>
-        <p className="text-xl mb-6 ">Looks like fell into a Black Hole. 😕</p>
-        <a href="/" className="text-center hover:text-neutral-300 duration-150">
-          Click to return to the Milky Way. 🌌
-        </a>
+      <div className="body">
+        <div className="w-fit mx-auto mt-32 p-2 text-left select-none  text-neutral-50 font-semibold">
+          <p className="text-4xl">Hmm.</p>
+          <p className="text-xl mb-6 ">Looks like fell into a Black Hole. 😕</p>
+          <a
+            href="/"
+            className="text-center hover:text-neutral-300 duration-150"
+          >
+            Click to return to the Milky Way. 🌌
+          </a>
+        </div>
       </div>
     </>
   );
