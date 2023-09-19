e4e25c19fa1fa2a30de3cbfefb36c13de33abe47
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-19 23:24:38 +0300

    "/about" & "/whatisblackhole" added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import Index from "./views/index";
 import BlackHoles from "./views/blackholes";
 import BlackHole from "./views/blackhole";
 import NotFound from "./views/notFound";
+import About from "./views/about";
+import WhatIsBlackHole from "./views/WhatIsBlackHole";
 import RandomBlackHole from "./views/random";
+
 function App() {
   return (
     <>
@@ -14,6 +17,8 @@ function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/blackholes" element={<BlackHoles />} />
           <Route path="/random" element={<RandomBlackHole />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/whatisblackhole" element={<WhatIsBlackHole />} />
           <Route path="blackhole">
             <Route path=":id" element={<BlackHole />} />
           </Route>
