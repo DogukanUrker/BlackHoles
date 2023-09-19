f03cd000cac0735c0ad22153ef3169760df49c28
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-19 23:24:49 +0300

    new pages added

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,27 +3,39 @@ export default function index() {
     <div className="body">
       <div className="w-full h-screen pt-36 text-center select-none  text-neutral-50 font-semibold backdrop-brightness-95">
         <p className="text-5xl mb-6 font-medium">Black Holes</p>
-        <br />
-        <a
-          href="/blackholes"
-          className="text-center hover:text-neutral-300 duration-150 text-xl"
-        >
-          See all Black Holes.
-        </a>{" "}
-        <br />
-        <a
-          href="/search"
-          className="text-center hover:text-neutral-300 duration-150 text-xl"
-        >
-          Search Black Holes.
-        </a>
-        <br />
-        <a
-          href="/random"
-          className="text-center hover:text-neutral-300 duration-150 text-xl"
-        >
-          Random Black Hole.
-        </a>
+        <div className="flex flex-col justify-center">
+          {" "}
+          <a
+            href="/blackholes"
+            className="text-center hover:text-neutral-300 duration-150 text-xl m-1 w-fit mx-auto"
+          >
+            See all Black Holes.
+          </a>
+          <a
+            href="/search"
+            className="text-center hover:text-neutral-300 duration-150 text-xl m-1 w-fit mx-auto"
+          >
+            Search Black Holes.
+          </a>
+          <a
+            href="/random"
+            className="text-center hover:text-neutral-300 duration-150 text-xl m-1 w-fit mx-auto"
+          >
+            Random Black Hole.
+          </a>
+          <a
+            href="/whatisblackhole"
+            className="text-center hover:text-neutral-300 duration-150 text-xl m-1 w-fit mx-auto"
+          >
+            What is a Black Hole?
+          </a>
+          <a
+            href="/about"
+            className="text-center hover:text-neutral-300 duration-150 text-xl m-1 w-fit mx-auto"
+          >
+            About.
+          </a>
+        </div>
       </div>
     </div>
   );
