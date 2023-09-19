157dd06890999853e346c14cb65ac237718d7715
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-19 20:42:48 +0300

    /search added

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,6 +1,6 @@
 export default function index() {
   return (
-    <div className="body ">
+    <div className="body">
       <div className="w-full h-screen pt-36 text-center select-none  text-neutral-50 font-semibold backdrop-brightness-95">
         <p className="text-5xl mb-6 font-medium">Black Holes</p>
         <br />
@@ -12,17 +12,17 @@ export default function index() {
         </a>{" "}
         <br />
         <a
+          href="/search"
+          className="text-center hover:text-neutral-300 duration-150 text-xl"
+        >
+          Click to search Black Holes.
+        </a>
+        <br />
+        <a
           href="/random"
           className="text-center hover:text-neutral-300 duration-150 text-xl"
         >
           Click for a random Black Hole.
-        </a>
-        <br />
-        <a
-          href="/go"
-          className="text-center hover:text-neutral-300 duration-150 text-xl"
-        >
-          Click to find Black Holes by ID.
         </a>
       </div>
     </div>
