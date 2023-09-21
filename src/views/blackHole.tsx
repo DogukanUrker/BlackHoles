0269c9c81edd147160fb1967546e752d5b4fb072
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-21 22:04:04 +0300

    added see all button to "/blackhole/{id}"

--- src/views/blackHole.tsx
+++ src/views/blackHole.tsx
@@ -14,5 +14,15 @@ export default function blackhole() {
       return blackHoleDetailedComponent(data);
     });
   };
-  return <div className="text-neutral-50">{renderData()}</div>;
+  return (
+    <div className="text-neutral-50">
+      <a
+        href="/blackholes"
+        className="p-2 text-neutral-50 bg-neutral-900 font-medium rounded hover:bg-neutral-800 duration-150 absolute top-2 left-2"
+      >
+        All
+      </a>
+      {renderData()}
+    </div>
+  );
 }
