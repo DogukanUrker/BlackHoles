8813e256903b89d74b280004e4350485ad2c84d3
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-21 21:49:21 +0300

    search bar added

--- src/views/blackHoles.tsx
+++ src/views/blackHoles.tsx
@@ -1,20 +1,33 @@
 import axios from "axios";
 import { API } from "../constants";
 import { useState, useEffect } from "react";
-import { blackholesComponent } from "../components/blackhole";
+import { blackHoleComponent } from "../components/blackHoleComponent";
 export default function blackholes() {
   const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
   useEffect(() => {
     axios.get(`${API}data`).then((json) => setData(json.data));
   }, []);
   const renderData = () => {
-    return data.map((data) => {
-      return blackholesComponent(data);
+    return data.map((blackhole) => {
+      if (blackhole.name[0].toLowerCase().includes(input.toLowerCase())) {
+        return blackHoleComponent(blackhole);
+      }
     });
   };
   return (
-    <div className="text-neutral-50 flex  flex-wrap justify-center">
-      {renderData()}
-    </div>
+    <>
+      <div className="mx-auto w-fit mt-4">
+        <input
+          type="text"
+          className="p-2 text-center rounded bg-neutral-900 text-neutral-50 focus:outline-0 focus:ring-1 focus:ring-sky-300 duration-150"
+          placeholder="search"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div className="text-neutral-50 flex  flex-wrap justify-center">
+        {renderData()}
+      </div>
+    </>
   );
 }
