2861e254cd0a4e3fa1cd81b5b69b7d504aff37d8
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-21 21:29:51 +0300

    data renaming

--- src/components/blackHoleComponent.tsx
+++ src/components/blackHoleComponent.tsx
@@ -1,29 +1,29 @@
-export function blackHoleComponent(data: string) {
+export function blackHoleComponent(blackhole: string) {
   return (
     <div className="bg-neutral-800 p-2 m-4 rounded w-[26rem] mx-auto">
       <div className="h-96 flex items-center">
         <img
-          src={data.image}
+          src={blackhole.image}
           className="w-64 mx-auto mb-2 rounded drop-shadow-md"
         />
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2 ">
         <p>Name:</p>{" "}
         <a
-          href={`/blackhole/${data.id}`}
+          href={`/blackhole/${blackhole.id}`}
           className="text-sky-300 hover:text-sky-400 duration-150"
         >
-          {data.name[0]}
+          {blackhole.name[0]}
         </a>
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>List:</p> {data.list}
+        <p>List:</p> {blackhole.list}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <a href={data.wikipedia} target="_blank">
+        <a href={blackhole.wikipedia} target="_blank">
           WikiPedia 📕
         </a>
-        <a href={data.map} target="_blank">
+        <a href={blackhole.map} target="_blank">
           Map 🗺️
         </a>
       </div>
--- src/components/blackHoleDetailedComponent.tsx
+++ src/components/blackHoleDetailedComponent.tsx
@@ -1,74 +1,74 @@
-export function blackHoleDetailedComponent(data: string) {
+export function blackHoleDetailedComponent(blackhole: string) {
   return (
     <div className="bg-neutral-800 p-2 m-4 rounded w-[26rem] mx-auto">
       <div>
-        <img src={data.image} className="w-64 mx-auto mb-2 rounded" />
+        <img src={blackhole.image} className="w-64 mx-auto mb-2 rounded" />
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Name:</p> {data.name[0]}
+        <p>Name:</p> {blackhole.name[0]}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>List:</p> {data.list}
+        <p>List:</p> {blackhole.list}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Kind:</p> {data.kind}
+        <p>Kind:</p> {blackhole.kind}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Type:</p> {data.type}
+        <p>Type:</p> {blackhole.type}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Age:</p> {data.age.text}
+        <p>Age:</p> {blackhole.age.text}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Constellation:</p> {data.constellation}
+        <p>Constellation:</p> {blackhole.constellation}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Right Ascension:</p> {data.rightAscension}
+        <p>Right Ascension:</p> {blackhole.rightAscension}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Declination:</p> {data.declination}
+        <p>Declination:</p> {blackhole.declination}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Coordinates:</p> {data.coordinates}
+        <p>Coordinates:</p> {blackhole.coordinates}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Redshift:</p> {data.redshift}
+        <p>Redshift:</p> {blackhole.redshift}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Apparent Magnitude:</p> {data.apparentMagnitude}
+        <p>Apparent Magnitude:</p> {blackhole.apparentMagnitude}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Absolute Magnitude:</p> {data.absoluteMagnitude}
+        <p>Absolute Magnitude:</p> {blackhole.absoluteMagnitude}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Discovery Year:</p> {data.discovery.year}
+        <p>Discovery Year:</p> {blackhole.discovery.year}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Discovery Location:</p> {data.discovery.location}
+        <p>Discovery Location:</p> {blackhole.discovery.location}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Discoverer:</p> {data.discovery.discoverer}
+        <p>Discoverer:</p> {blackhole.discovery.discoverer}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Solar Mass:</p> {data.solarmass.text}
+        <p>Solar Mass:</p> {blackhole.solarmass.text}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Radius:</p> {data.radius}
+        <p>Radius:</p> {blackhole.radius}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Luminosity:</p> {data.luminosity}
+        <p>Luminosity:</p> {blackhole.luminosity}
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Temperature:</p> {data.temperature.kelvin} K
+        <p>Temperature:</p> {blackhole.temperature.kelvin} K
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <p>Distance:</p> {data.distance.ly} Light Year
+        <p>Distance:</p> {blackhole.distance.ly} Light Year
       </div>
       <div className="flex justify-between bg-neutral-900 p-2 rounded m-2">
-        <a href={data.wikipedia} target="_blank">
+        <a href={blackhole.wikipedia} target="_blank">
           WikiPedia 📕
         </a>
-        <a href={data.map} target="_blank">
+        <a href={blackhole.map} target="_blank">
           Map 🗺️
         </a>
       </div>
