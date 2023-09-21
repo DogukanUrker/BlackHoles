f0dc69956d432ad107b4f3b786ec5039e472195c
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-21 22:36:05 +0300

    image size changes and hover scale effects

--- src/components/blackHoleComponent.tsx
+++ src/components/blackHoleComponent.tsx
@@ -1,11 +1,11 @@
 export function blackHoleComponent(blackhole: string) {
   return (
-    <div className="bg-neutral-900 p-2 m-4 rounded mx-auto">
-      <div className="h-fit flex flex-col items-center ">
+    <div className="bg-neutral-900 p-2 my-4 mx-6 rounded hover:scale-110 duration-200">
+      <div className="h-fit flex flex-col items-center">
         <a href={`/blackhole/${blackhole.id}`}>
           <img
             src={blackhole.image}
-            className="w-64 h-64 mx-auto mb-2 rounded drop-shadow-md"
+            className="w-56 h-56 mx-auto mb-2 rounded drop-shadow-md hover:scale-105 duration-200"
           />
         </a>
         <a
