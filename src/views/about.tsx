482a1822102b1c0f3c5a7156671e5d9d44622aec
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-21 21:53:33 +0300

    added routing "/whatisblackhole" to 'blackhole' text

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -5,12 +5,30 @@ export default function About() {
         <div className="backdrop-blur-sm rounded m-2 p-4 w-6/12 mx-auto mt-36 text-neutral-50 select-none text-center">
           <h1 className="text-5xl mb-6 font-medium">About</h1>
           <p className="text-xl">
-            This list of <a href="/whatisblackhole">black holes</a> (and stars
-            considered probable candidates) is organized by mass (including
-            <a href="/whatisblackhole">black holes</a> of undetermined mass);
-            some items in this site are galaxies or star clusters that are
-            believed to be organized around a
-            <a href="/whatisblackhole">black hole</a>.
+            This list of{" "}
+            <a
+              href="/whatisblackhole"
+              className="text-sky-300 hover:text-sky-400 duration-150"
+            >
+              black holes
+            </a>{" "}
+            (and stars considered probable candidates) is organized by mass
+            (including{" "}
+            <a
+              href="/whatisblackhole"
+              className="text-sky-300 hover:text-sky-400 duration-150"
+            >
+              black holes
+            </a>{" "}
+            of undetermined mass); some items in this site are galaxies or star
+            clusters that are believed to be organized around a{" "}
+            <a
+              href="/whatisblackhole"
+              className="text-sky-300 hover:text-sky-400 duration-150"
+            >
+              black hole
+            </a>
+            .
           </p>
           <p className="text-xs mt-4">
             Made with 💖 by{" "}
